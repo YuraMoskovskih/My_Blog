08d938df642517ce5c28987acce7b7824b65a2de
Yura Moskovskih <yuramoskovskih@gmail.com>
2020-03-23 23:38:58 +0300

    Version 1.2

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
-from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
-from django.shortcuts import redirect
+from .models import Post, Comment
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm, CommentForm
 
 
 def post_list(request):
@@ -12,7 +11,22 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,
+		  'blog/post_detail.html',
+		  {'post': post,
+		  'comments': comments,
+		  'new_comment': new_comment,
+		  'comment_form': comment_form})
 
 def post_new(request):
     if request.method == "POST":
