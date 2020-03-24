4e9058e2e307a5eaa78501e1d45f743b15a1b2c4
Yura Moskovskih <yuramoskovskih@gmail.com>
2020-03-24 07:58:21 +0300

    + Image

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
